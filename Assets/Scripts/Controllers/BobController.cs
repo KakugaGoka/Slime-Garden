85094bb97b94c8d89e82e7b90f167543ff29b904
KakugaGoka <KakugaGoka@live.com>
2020-10-22 02:44:23 -0700

    Added in the Slime Station where you can name your slime. May also be where you enter races later on...

---
+++ Assets/Scripts/Controllers/BobController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+public class BobController : MonoBehaviour {
+    float startingYPosition;
+
+    public float amplitude = 1; 
+
+    void Start() {
+        this.startingYPosition = this.transform.position.y;
+    }
+
+    void Update() {
+        transform.position = new Vector3(transform.position.x, startingYPosition + ((float)Math.Sin(Time.time) * amplitude), transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Controllers/SlimeStationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlimeStationController : MonoBehaviour
+{
+    public InputField nameField;
+    public Button nameButton;
+
+    private SlimeController m_Slime;
+    private InteractController m_InteractController;
+
+    void Awake() {
+        m_InteractController = GetComponent<InteractController>();
+        m_InteractController.onInteract += SetUp;
+    }
+
+    private void SetUp(PlayerCharacterController player) {
+        if (player.heldItem) {
+            m_Slime = player.heldItem.GetComponent<SlimeController>();
+            if (m_Slime) {
+                OnFound(m_Slime);
+            } else {
+                OnFail();
+            }
+        } else {
+            OnFail();
+        }
+    }
+
+    private void OnFail() {
+        nameField.text = "No slime detected... Have a nice day!";
+        nameField.interactable = false;
+        nameButton.interactable = false;
+    }
+
+    private void OnFound(SlimeController slime) {
+        nameField.placeholder.enabled = false;
+        nameField.text = slime.name;
+        nameField.interactable = true;
+        nameButton.interactable = true;
+    }
+
+    public void Rename() {
+        if (m_Slime) {
+            m_Slime.name = nameField.text;
+        }
+    }
+}
